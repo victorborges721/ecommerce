f523ab0e4c6a60e44de2a03f9c69f9b1abe268b1
Victor Borges <victor.borges.721@gmail.com>
2021-09-08 14:21:52 -0400

    Deleted unneeded files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
 
 function App() {
-  return (
-    <Router>
-      <Home />
-    </Router>
-  );
+  return <div></div>;
 }
 
 export default App;
