852f667ee9b9359c7438badc3fe388698b2db0d4
Victor Borges <victor.borges.721@gmail.com>
2021-09-08 12:05:00 -0400

    Added productSchema, list of products, and seederScript.js

---
+++ backend/data/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    name: "Fire Manipulation",
+    description: "The power to manipulate fire.",
+    price: 0,
+    category: "Elemental",
+    imageUrl:
+      "https://static.wikia.nocookie.net/powerlisting/images/d/d8/Roku.gif/revision/latest/scale-to-width-down/516?cb=20180707031529",
+  },
+  {
+    name: "Weather Manipulation",
+    description: "The power to manipulate the weather.",
+    price: 0,
+    category: "Elemental",
+    imageUrl:
+      "https://static.wikia.nocookie.net/powerlisting/images/5/5f/Storm_Blizzard.png/revision/latest/scale-to-width-down/333?cb=20131027223914",
+  },
+  {
+    name: "Teleportation",
+    description:
+      "The power to move instantaneously from one location to another without physically occupying the space in between.",
+    price: 0,
+    category: "Transportation",
+    imageUrl:
+      "https://static.wikia.nocookie.net/powerlisting/images/d/dd/Nightcrawlerteleport.gif/revision/latest/scale-to-width-down/350?cb=20160110200119",
+  },
+  {
+    name: "Clairvoyance",
+    description:
+      "The power to acquire information using sight outside the normal range of detection.",
+    price: 0,
+    category: "Extra-Sensory Perception (ESP)",
+    imageUrl:
+      "https://static.wikia.nocookie.net/powerlisting/images/f/fa/Looking_for_Alejandro.jpg/revision/latest/scale-to-width-down/350?cb=20100703215307",
+  },
+  {
+    name: "Reactive Adaptation",
+    description:
+      "The ability to develop adaptations, powers, or skills in response to immediate threats.",
+    price: 0,
+    category: "Adaptation",
+    imageUrl:
+      "https://static.wikia.nocookie.net/powerlisting/images/d/d6/X-Treme_X-Men_Vol_1_9_Textless.jpg/revision/latest/scale-to-width-down/333?cb=20110812222148",
+  },
+  {
+    name: "Sonic Scream",
+    description:
+      "The power to emit highly enhanced scream of a high amplitude.",
+    price: 0,
+    category: "Emission",
+    imageUrl:
+      "https://static.wikia.nocookie.net/powerlisting/images/3/3d/Gotenks_%28Dragon_Ball_Z%29_Using_Vice_Shout.gif/revision/latest/scale-to-width-down/516?cb=20200725044354",
+  },
+];
+
+module.exports = products;
