548faa85a8732a01cb453ef9be263aed255f3aa6
Victor Borges <victor.borges.721@gmail.com>
2021-09-17 18:04:34 -0400

    removed unneeded import

--- frontend/src/screens/PaymentScreen/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen/PaymentScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Container, Form, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { months, days } from "../../assets/vars";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "./style.css";
 
 // Actions
